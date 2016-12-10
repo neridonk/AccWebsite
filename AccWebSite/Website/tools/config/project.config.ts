23c4dffac6b807cfc010cfec8ce63aff531695bc
neridonk <neridonk@googlemail.com>
2016-12-10 12:16:36 +0100

    fix jq

--- AccWebSite/Website/tools/config/project.config.ts
+++ AccWebSite/Website/tools/config/project.config.ts
@@ -21,7 +21,6 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, vendor: false },
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
@@ -29,6 +28,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `local` third-party libraries to be injected/bundled.
     this.APP_ASSETS = [
       ...this.APP_ASSETS,
+      { src: `${this.CSS_SRC}/jquery.min.js`, inject: true, vendor: false},
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
